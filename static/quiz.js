d5b9b2cd9a7b40cf22fb11a0af7987795465c84f
Maria Teresa Tome <55467678+mteretome@users.noreply.github.com>
2022-04-24 22:48:37 -0400

    made shirt+pant more aligned

--- static/quiz.js
+++ static/quiz.js
@@ -149,7 +149,7 @@ function outfit_grid(images){
     top.append(' <div class="leftarm">')
     top.append(' <div class="rightarm">')
 
-    let bottom = $(' <div class="col-md-12  parent" id="bottom">')
+    let bottom = $(' <div class="col-md-12 parent" id="bottom">')
     bottom.append(' <div class="leftleg">')
     bottom.append(' <div class="rightleg">')
 
@@ -171,7 +171,7 @@ function outfit_grid(images){
             // top.append(' <div class="torso">')
             // top.append(' <div class="leftarm">')
             // top.append(' <div class="rightarm">')
-            top.append(drag).attr("class", "topp")
+            top.append(drag).attr("class", "topp").addClass("pr-3")
             if(!submission.includes(drag.attr('alt'))){
                 submission[0]=drag.attr('alt');
             } 
@@ -189,7 +189,7 @@ function outfit_grid(images){
             drag.removeAttr("style");
             bottom.text("")
            
-            bottom.append(drag).attr("class", "pants")
+            bottom.append(drag).attr("class", "pants").addClass("pl-4")
             if(!submission.includes(drag.attr('alt'))){
                 submission[1]=drag.attr('alt');
             } 
